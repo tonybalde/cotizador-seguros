66e58e74c1ee3fe88f92a6950a7f950ecb34eafb
Tony Baldessari <tonybalde@hotmail.com>
2023-10-31 21:12:50 -0300

    Limpiamos codigo viejo

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import Header from './components/Header';
-// import Form from './components/Form';
-// import Historial from './components/Historial';
-// import Footer from './components/Footer';
-// import './App.css';
-
-// function App() {
-//   return (
-    
-    
-//     <Router>
-//     <div className="main-app-container">
-//         <Header />
-//         <Switch>
-//           <Route path="/" component={Form} />
-//           <Route path="/historial" component={Historial} />
-//         </Switch>
-//         <Footer />
-//       </div>
-//     </Router>
-      
-    
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
